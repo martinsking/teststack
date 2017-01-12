8d89245c35af22b3a94acd7c1495183927d4b069
martinsking <mking@localhost>
2017-01-12 21:19:34 +0000

    Now Corrected to Book

--- src/teststack/CharStack.java
+++ src/teststack/CharStack.java
@@ -16,12 +16,14 @@ public class CharStack {
     //Constructor:
     public CharStack(int capacity) {
         stackArray = new char[capacity];
-        topOfStack =-1;
+        topOfStack = -1;
         
     }
     
     // Methods@
     public void push(char element ) { stackArray[++topOfStack] = element; }
-    public char pop() { return stackArray[topOfStack]; } // should this have a - at then end
-    // needs finishing
+    public char pop() { return stackArray[topOfStack--]; } 
+    public char peek() { return stackArray[topOfStack];}
+    public boolean isEmpty() {return topOfStack == -1;}
+    public boolean isFull() {return topOfStack == stackArray.length - 1;}
 }
